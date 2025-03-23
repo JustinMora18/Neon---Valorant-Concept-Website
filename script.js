6c2db92a9bc8c80687c24793153879dba049eea9
JustinMora18 <justindaniel1810@gmail.com>
2025-03-22 20:20:39 -0400

    adjust z-index ; change nav list, main text, background colors; modify neon img

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header"); // Declara la variable header
+    
     function setupModal(modalNumber) {
         const openBtn = document.getElementById(`openModal${modalNumber}`);
         const closeBtn = document.getElementById(`closeModal${modalNumber}`);
@@ -6,19 +8,22 @@ document.addEventListener("DOMContentLoaded", function () {
     
         openBtn.addEventListener("click", () => {
             modal.classList.add("open");
+            header.style.zIndex = "-2"; // Baja el z-index del header
         });
     
         closeBtn.addEventListener("click", () => {
             modal.classList.remove("open");
+            header.style.zIndex = "999"; // Restaura el z-index del header
         });
     
         window.addEventListener("click", (event) => {
             if (event.target === modal) {
                 modal.classList.remove("open");
+                header.style.zIndex = "1000";
             }
         });
     }
-
+    
     for (let i = 1; i <= 4; i++) {
         setupModal(i);
     }
